46340267191d10ede55ca8667fac5cab51b39954
paulafiaschi <pau.fiaschi@gmail.com>
2023-08-15 11:09:03 +0200

    tokens styling

--- components/GameBoard.jsx
+++ components/GameBoard.jsx
@@ -16,11 +16,23 @@ export default function GameBoard() {
   const [gameState, setGameState] = useState(initialBoard);
   const [currentPlayer, setCurrentPlayer] = useState(1);
 
+  const addToken = (columnI) => {
+    const column = gameState[columnI];
+    const tokenPos = column.indexOf(null);
+
+    tokenPos !== -1
+      ? ((column[tokenPos] = currentPlayer),
+        setCurrentPlayer(currentPlayer === 1 ? 2 : 1))
+      : null;
+
+    // console.log(gameState);
+  };
+
   return (
     <main className={Styles.main}>
       <div className={Styles.board}>
         {gameState.map((col, i) => {
-          return <GameColumn col={col} key={i} onClick={() => addToken()} />;
+          return <GameColumn col={col} key={i} onClick={() => addToken(i)} />;
         })}
       </div>
     </main>
--- components/GameColumn.jsx
+++ components/GameColumn.jsx
@@ -4,7 +4,20 @@ export default function GameColumn({ col, i, onClick }) {
   return (
     <div className={Styles.column}>
       {col.map((cell, i) => {
-        return <span className={Styles.gameCell} key={`cell-${i}`}></span>;
+        let color = "whiteCircle";
+
+        if (cell === 1) {
+          color = "redCircle";
+        } else if (cell === 2) {
+          color = "yellowCircle";
+        }
+        return (
+          <span
+            className={`${Styles.gameCell} ${Styles[color]}`}
+            key={`cell-${i}`}
+            onClick={onClick}
+            value={cell}></span>
+        );
       })}
     </div>
   );
